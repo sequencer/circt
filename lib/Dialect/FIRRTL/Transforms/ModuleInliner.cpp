c264cd6f39b4d2d82225f567f62cfc5a44e30e6e
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-01-13 18:44:11 -0500

    [FIRRTL] Add MutableNLA to ModuleInliner pass
    Adds a utility class for storing updates to a non-local annotation
    called MutableNLA.  This is intended to help tracking updates to a
    non-local annotation while inlining/flattening occurs.  This commit does
    not update the ModuleInliner pass to use this utility, yet.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- lib/Dialect/FIRRTL/Transforms/ModuleInliner.cpp
+++ lib/Dialect/FIRRTL/Transforms/ModuleInliner.cpp
@@ -16,17 +16,303 @@
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
 #include "circt/Dialect/FIRRTL/FIRRTLTypes.h"
 #include "circt/Dialect/FIRRTL/FIRRTLVisitors.h"
+#include "circt/Dialect/FIRRTL/Namespace.h"
 #include "circt/Dialect/FIRRTL/Passes.h"
+#include "circt/Dialect/HW/HWAttributes.h"
 #include "circt/Support/LLVM.h"
 #include "mlir/IR/BlockAndValueMapping.h"
+#include "llvm/ADT/BitVector.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/FormatVariadic.h"
 
 using namespace circt;
 using namespace firrtl;
 using namespace chirrtl;
 
+using llvm::BitVector;
+
 //===----------------------------------------------------------------------===//
 // Module Inlining Support
 //===----------------------------------------------------------------------===//
+
+namespace {
+/// A representation of an NLA that can be mutated.  This is intended to be used
+/// in situations where you want to make a series of modifications to an NLA
+/// while also being able to query information about it.  Finally, the NLA is
+/// written back to the IR to replace the original NLA.
+class MutableNLA {
+  // Storage of the NLA this represents.
+  NonLocalAnchor nla;
+
+  // A namespace that can be used to generate new symbol names if needed.
+  CircuitNamespace *circuitNamespace;
+
+  /// A mapping of symbol to index in the NLA.
+  DenseMap<Attribute, unsigned> symIdx;
+
+  /// Records which elements of the path are inlined.
+  BitVector inlinedSymbols;
+
+  /// The point after which the NLA is flattened.
+  signed flattenPoint = -1;
+
+  /// Indicates if this NLA is dead and should be deleted.
+  bool dead = false;
+
+  /// Stores any new roots for the NLA.  This occurs when an NLA's root is
+  /// inlined and one new NLA is created for each instance of the inlined root.
+  SmallVector<std::pair<StringAttr, StringAttr>> newTops;
+
+  /// Stores the size of the NLA path.
+  unsigned int size;
+
+public:
+  MutableNLA(NonLocalAnchor nla, CircuitNamespace *circuitNamespace)
+      : nla(nla), circuitNamespace(circuitNamespace),
+        inlinedSymbols(BitVector(nla.namepath().size(), true)),
+        size(nla.namepath().size()) {
+    llvm::errs() << "Constructing Mutable NLA for: " << nla << "\n"
+                 << "  symIdx:\n";
+    for (size_t i = 0, e = size; i != e; ++i) {
+      symIdx.insert({modPart(i), i});
+      llvm::errs() << "    - " << modPart(i) << " -> " << i << "\n";
+    }
+  }
+
+  /// This default constructor only exists so that the `[]` referential lookup
+  /// can be used for `Inliner::nlaMap`.  The `nlaMap` is prepopulated with all
+  /// NLAs, so this will always hit.
+  MutableNLA() {
+    llvm_unreachable(
+        "the default constructor for MutableNLA should never be used");
+  }
+
+  /// Return the original NLA that this was pointing at.
+  NonLocalAnchor getNLA() { return nla; }
+
+  /// Writeback updates accumulated in this MutableNLA to the IR.  This method
+  /// should only ever be called once and, if a writeback occurrs, the
+  /// MutableNLA is NOT updated for further use.  Interacting with the
+  /// MutableNLA in any way after calling this method may result in crashes.
+  /// (This is done to save unnecessary state cleanup of a pass-private
+  /// utility.)
+  NonLocalAnchor applyUpdates() {
+    // Delete an NLA which is either dead or has been made local.
+    if (isLocal() || isDead()) {
+      nla.erase();
+      return nullptr;
+    }
+
+    // The NLA was never updated, just return the NLA and do not writeback
+    // anything.
+    if (inlinedSymbols.all() && newTops.empty() && flattenPoint == -1)
+      return nla;
+
+    // The NLA has updates.  Generate a new NLA with the same symbol and delete
+    // the original NLA.
+    OpBuilder b(nla);
+    auto writeBack = [&](StringAttr root, StringAttr sym) -> NonLocalAnchor {
+      SmallVector<Attribute> namepath;
+      StringAttr lastMod;
+      // This unrolls the first iteration of the loop because it has weird
+      // behavior around using the root name.
+      if (!inlinedSymbols.test(1)) {
+        if (!lastMod)
+          lastMod = root;
+      } else {
+        namepath.push_back(hw::InnerRefAttr::get(
+            nla.getContext(), lastMod ? lastMod : root, refPart(0)));
+      }
+      // Rest of the loop.
+      for (signed i = 1, e = inlinedSymbols.size() - 1; i != e; ++i) {
+        if (i == flattenPoint) {
+          lastMod = modPart(i);
+          break;
+        }
+
+        if (!inlinedSymbols.test(i + 1)) {
+          if (!lastMod)
+            lastMod = modPart(i);
+          continue;
+        }
+
+        namepath.push_back(hw::InnerRefAttr::get(
+            nla.getContext(), lastMod ? lastMod : modPart(i), refPart(i)));
+        lastMod = {};
+      }
+
+      if (flattenPoint == -1)
+        namepath.push_back(nla.namepath()[size - 1]);
+      else
+        namepath.push_back(hw::InnerRefAttr::get(nla.getContext(), lastMod,
+                                                 refPart(size - 1)));
+
+      return b.create<NonLocalAnchor>(b.getUnknownLoc(), sym,
+                                      b.getArrayAttr(namepath));
+    };
+
+    NonLocalAnchor last;
+    assert(!dead || !newTops.empty());
+    if (!dead)
+      last = writeBack(root(), nla.getNameAttr());
+    for (auto root : newTops)
+      last = writeBack(root.first, root.second);
+
+    nla.erase();
+    return last;
+  }
+
+  /// Write this MutableNLA to a stream using FIRRTL target syntax.  The target
+  /// is written with all updates applied.
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os, MutableNLA &x) {
+    auto writeOne = [&](StringRef root) {
+      os << "~" << x.nla->getParentOfType<CircuitOp>().name() << "|";
+      auto namepath = x.nla.namepath();
+      os << root;
+
+      SmallString<32> prefix;
+      for (signed i = 1, e = x.inlinedSymbols.size(); i != e; ++i) {
+        if (i - 1 == x.flattenPoint)
+          break;
+
+        StringAttr mod = x.modPart(i);
+        StringAttr ref;
+        if (auto innerRef = namepath[i - 1].dyn_cast<hw::InnerRefAttr>())
+          ref = innerRef.getName();
+
+        if (!prefix.empty() && ref)
+          prefix.append("_");
+        prefix.append(ref);
+        if (!x.inlinedSymbols.test(i))
+          continue;
+
+        os << "/" << prefix << ":";
+        os << mod.getValue();
+        prefix.clear();
+      }
+
+      if (auto innerRef =
+              namepath[namepath.size() - 1].dyn_cast<hw::InnerRefAttr>()) {
+        if (!prefix.empty())
+          prefix.append("_");
+        prefix.append(innerRef.getName());
+        os << ">" << prefix;
+      }
+    };
+
+    SmallVector<Attribute> tops;
+    if (!x.dead)
+      tops.push_back(x.root());
+    auto newTopNames = llvm::map_range(
+        x.newTops, [](std::pair<Attribute, Attribute> a) { return a.first; });
+    tops.append(newTopNames.begin(), newTopNames.end());
+
+    bool multiary = !x.newTops.empty();
+    if (multiary)
+      os << "[";
+    llvm::interleaveComma(tops, os,
+                          [&](Attribute a) { writeOne(a.cast<StringAttr>()); });
+    if (multiary)
+      os << "]";
+
+    return os;
+  }
+
+  /// Returns true if this NLA is dead.  There are several reasons why this
+  /// could be dead:
+  ///   1. This NLA has no uses and was not re-topped.
+  ///   2. This NLA was flattened and its leaf reference is a Module.
+  bool isDead() { return dead && newTops.empty(); }
+
+  /// Returns true if this NLA is local.  For this to be local, every module
+  /// after the root (up to the flatten point or the end) must be inlined.  The
+  /// root is never truly inlined as inlined as inlining the root just sets a
+  /// new root.
+  bool isLocal() {
+    unsigned end = flattenPoint > -1 ? flattenPoint + 1 : inlinedSymbols.size();
+    return inlinedSymbols.find_first_in(1, end) == -1;
+  }
+
+  /// Return the module part at a specific index in the original NLA.
+  StringAttr modPart(unsigned i) {
+    return TypeSwitch<Attribute, StringAttr>(nla.namepath()[i])
+        .Case<FlatSymbolRefAttr>([](auto a) { return a.getAttr(); })
+        .Case<hw::InnerRefAttr>([](auto a) { return a.getModule(); });
+  }
+
+  /// Return the module where the NLA starts.
+  StringAttr root() {
+    assert(!nla.namepath().empty());
+    return modPart(0);
+  }
+
+  /// Return the reference part at a specific index in the original NLA.
+  StringAttr refPart(unsigned i) {
+    return TypeSwitch<Attribute, StringAttr>(nla.namepath()[i])
+        .Case<FlatSymbolRefAttr>([](auto a) { return a.getAttr(); })
+        .Case<hw::InnerRefAttr>([](auto a) { return a.getName(); });
+  }
+
+  /// Return the name of the reference pointed at by the NLA.
+  StringRef ref() {
+    assert(!nla.namepath().empty());
+    return refPart(size - 1);
+  }
+
+  /// Mark a module as inlined.  This will remove it from the NLA.
+  void inlineModule(FModuleOp module) {
+    auto sym = module.getNameAttr();
+    assert(sym != root() && "unable to inline the root module");
+    assert(symIdx.count(sym) && "module is not in the symIdx map");
+    auto idx = symIdx[sym];
+    llvm::errs() << "Inlining " << sym << ", index: " << idx << "\n"
+                 << "  old state: ";
+    for (unsigned long x : inlinedSymbols.getData())
+      llvm::errs() << llvm::formatv("{0:x}", x);
+    llvm::errs() << "\n  new state: ";
+    inlinedSymbols.reset(idx);
+    for (auto x : inlinedSymbols.getData())
+      llvm::errs() << llvm::formatv("{0:x}", x);
+    llvm::errs() << "\n";
+    // If we inlined the last module in the path, then this NLA is dead.
+    if (idx == size - 1)
+      dead = true;
+  }
+
+  /// Mark a module as flattened.  This has the effect of inlining all of its
+  /// children.  Also mark the NLA was dead if the leaf reference of this NLA is
+  /// a module.
+  void flattenModule(FModuleOp module) {
+    auto sym = module.getNameAttr();
+    assert(symIdx.count(sym) && "module is not in the symIdx map");
+    auto idx = symIdx[sym] - 1;
+    llvm::errs() << "Setting flatten point to: " << idx << "\n";
+    flattenPoint = idx;
+    // If the leaf reference is a module and we're flattening the NLA, then the
+    // NLA must be dead.  Mark it as such.
+    if (nla.namepath()[size - 1].isa<FlatSymbolRefAttr>())
+      dead = true;
+  }
+
+  StringAttr reTop(FModuleOp module) {
+    StringAttr sym = nla.sym_nameAttr();
+    if (!newTops.empty())
+      sym = StringAttr::get(nla.getContext(),
+                            circuitNamespace->newName(sym.getValue()));
+    llvm::errs() << nla.sym_name() << " adds top {" << module.getName() << ", "
+                 << sym.getValue() << "}\n";
+    newTops.push_back({module.getNameAttr(), sym});
+    dead = true;
+    return sym;
+  }
+
+  ArrayRef<std::pair<StringAttr, StringAttr>> getAdditionalSymbols() {
+    return llvm::makeArrayRef(newTops);
+  }
+
+  void markDead() { dead = true; }
+};
+} // namespace
 
 /// If this operation or any child operation has a name, add the prefix to that
 /// operation's name.
